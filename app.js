60fbcb875adbf21f185020f8fa4ab3b97757ddf4
Vinicius Santana <santanaviny@gmail.com>
2017-10-02 13:56:58 -0700

    A direction..

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+const _ = require('lodash');
+const os = require('os');
+const yargs = require('yargs');
+
+
+var user = os.userInfo().username;
+console.log(`Hello ${user}!`);
+
+var argv = yargs.argv;
+var command = argv._[0];
+
+if (command === 'add') {
+  console.log('Adding a new universe');
+} else if (command === 'list') {
+  console.log('Listing all universes');
+} else if (command === 'get') {
+  console.log('Getting an universe');
+} else if (command === 'remove') {
+  console.log('Removing an universe');
+} else {
+  console.log('Command not reconized')
+}
